7c9f97b77658511d4533d74da8d4f9e7c1baf773
SanTech <adhityahasan16@mhs.mdp.ac.id>
2022-05-31 19:18:41 +0700

    auth bersambung

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,15 @@ Route::get('/', function () {
 Route::get("/prodi/create", [ProdiController::class, "create"])->name("prodi.create");
 Route::post("/prodi/store", [ProdiController::class, "store"])->name("prodi.store");
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/prodi', [ProdiController::class, 'index'])->name('prodi.index'); //Route menampilkan semua data program studi
 Route::get('/prodi/{prodi}', [ProdiController::class, 'show'])->name('prodi.show'); //Route menampilkan satu data (detail) program studi
 
 Route::get('/prodi/{prodi}/edit', [ProdiController::class, 'edit'])->name('prodi.edit'); //Route untuk mengedit data
 Route::patch('/prodi/{prodi}', [ProdiController::class, 'update'])->name('prodi.update'); //Route untuk mengupdate data
 
+require __DIR__.'/auth.php';
 Route::delete('/prodi/{prodi}', [ProdiController::class, 'destroy'])->name('prodi.destroy'); //Route untuk menghapus data
